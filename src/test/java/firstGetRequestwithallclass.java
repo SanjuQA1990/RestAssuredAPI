53044f36302f4d30231174e85be943f4ae1ac889
SanjuQA1990 <sanjaykumar.emmbros@gmail.com>
2025-01-30 07:24:44 +0530

    new test without resources clss insted complete package

--- src/test/java/firstGetRequestwithallclass.java
+++ src/test/java/firstGetRequestwithallclass.java
@@ -1,22 +1,22 @@
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 
-public class firstGetRequest {
+public class firstGetRequestwithallclass {
 	
 	// https://reqres.in/api/users/2
 	@Test
 	void testCase(){
-		Response res= RestAssured.get("https://reqres.in/api/users/2");
+		Response res= get("https://reqres.in/api/users/2");
 		System.out.println(res.asString());
 		System.out.println(" Status Code= " + res.getStatusCode());
 	}
 	@Test
 	void test2() {
 		
-		RestAssured.baseURI= "https://reqres.in/api/users";
-		RestAssured .given().queryParams("Page","2").when().get().then().statusCode(200);		
+		baseURI= "https://reqres.in/api/users";
+		given().queryParams("Page","2").when().get().then().statusCode(200);		
 	}
 
 }
